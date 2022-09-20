80c1a1e4c3d86419c3a7d6d1b749624ddf85421b
param-v <112659738+param-v@users.noreply.github.com>
2022-09-20 14:48:23 -0600

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hi");
+// Color Array by Mr. V
+
+// Variables for HTML Elements
+let containerEl = document.getElementById("container")
+
+// Array
+let colors = ["red", "green", "blue"];
+// index ->     0       1        2
+
+containerEl.innerHTML = "<div>" + colors[0] + "</div>"
+
+console.log(colors[2]);
