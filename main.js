c4026af254bd21eeaa317df6aaa63ded75057575
param-v <112659738+param-v@users.noreply.github.com>
2022-09-21 14:48:51 -0600

    main.js

--- main.js
+++ main.js
@@ -7,6 +7,5 @@ let containerEl = document.getElementById("container")
 let colors = ["red", "green", "blue"];
 // index ->     0       1        2
 
-containerEl.innerHTML = "<div>" + colors[0] + "</div>"
+containerEl.innerHTML = "<div style='background: " + colors[0] + "'></div>";
 
-console.log(colors[2]);
