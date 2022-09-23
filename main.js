fd49b4ef690a2a91ae476ab9062ca28153dd1a04
param-v <112659738+param-v@users.noreply.github.com>
2022-09-23 14:48:01 -0600

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-// Color Array by Mr. V
+// Color Array by Param
 
 // Variables for HTML Elements
 let containerEl = document.getElementById("container")
 
 // Array
-let colors = ["red", "green", "blue"];
-// index ->     0       1        2
+let colors = ["red", "green", "blue", "orange", "cyan", "purple"];
+// index ->     0       1        2       3         5
 
-containerEl.innerHTML = "<div style='background: " + colors[0] + "'></div>";
+console.log(colors.length);
+
+//Click Event
+document.addEventListener("click", update);
+
+function update() {
+    containerEl.innerHTML = `<div style="background: ${colors[randomInt(0, colors.length)]}"></div>`;
+
+}
+
 
