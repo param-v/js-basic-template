d50799f761a4d4a2cc987d1cbc1df42762969263
param-v <112659738+param-v@users.noreply.github.com>
2022-10-03 14:49:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,11 @@ let colors = ["red", "green", "blue", "orange", "cyan", "purple"];
 // index ->     0       1        2       3         5
 
 // Display all colors on page
-for (let i = 0; i < 6; i++) {
-    let divStr = "";
+let divStr = "";
+for (let i = 0; i < colors.length; i++) {
     divStr += `<div style="background: ${colors[i]}"></div>`;
-    
 }
-
+containerEl.innerHTML = divStr;
 
 
 
