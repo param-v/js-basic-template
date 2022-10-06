4ab52b3db1826a21bb214e429d375a9e58cc5d16
param-v <112659738+param-v@users.noreply.github.com>
2022-10-06 14:48:39 -0600

    main.js

--- main.js
+++ main.js
@@ -2,17 +2,26 @@
 
 // Variables for HTML Elements
 let containerEl = document.getElementById("container")
+let inputEl = document.getElementById("color-in");
 
-// Array
-let colors = ["red", "green", "blue", "orange", "cyan", "purple"];
-// index ->     0       1        2       3         5
+// Event Listener
+inputEl.addEventListener("keydown", submitHandler);
 
-// Display all colors on page
-let divStr = "";
-for (let i = 0; i < colors.length; i++) {
-    divStr += `<div style="background: ${colors[i]}"></div>`;
+function submitHandler(event) {
+   if (event.keyCode === 13) {
+    // Create color array based on user input
+    let inputStr = inputEl.value;
+    let colorsArray = inputStr.split(",");
+    displayColors(colorsArray);
+   }
 }
-containerEl.innerHTML = divStr;
 
-
+function displayColors(colors) {
+    // Display all colors on page
+    let divStr = "";
+    for (let i = 0; i < colors.length; i++) {
+        divStr += `<div style="background: ${colors[i]}"></div>`;
+    }
+    containerEl.innerHTML = divStr;
+}
 
